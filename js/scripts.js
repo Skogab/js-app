8e53c5c0e78e22faaf66e487d024aff55f8e5ed5
Skogab <114601288+Skogab@users.noreply.github.com>
2022-11-01 18:59:45 +0100

    changing index

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,1 @@
-let myName = "Bob";
-document.write(myName);
-myName = "John Doe";
-document.write(myName);
-
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
+alert("Hello world");
